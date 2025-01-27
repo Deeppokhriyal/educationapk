19dde5275f9227280ad096eab1259c7c1cc7bcb6
deepc <dpokhriyal624@gmail.com>
2025-01-27 16:13:32 +0530

    Uploaded

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,7 +1,6 @@
 import 'package:educationapk/allpage.dart';
 import 'package:educationapk/profilepage.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 
 class Myhome extends StatefulWidget {

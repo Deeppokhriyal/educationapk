56aa03b44b781711c54afb2ae76fda12b3aaffd6
Deeppokhriyal <dpokhriyal624@gmail.com>
2025-01-28 12:00:38 +0530

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:educationapk/login.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get_navigation/src/root/get_material_app.dart';
+
 
 
 void main(){
-  runApp(GetMaterialApp(
+  runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     initialRoute: 'login',
     routes: {

60eadb6cc2de58857b540711d8ed48aed96af6bb
deepc <dpokhriyal624@gmail.com>
2025-02-05 16:23:39 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:educationapk/login.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 

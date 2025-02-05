569c450fdde6a7055a3c624b4ed9d68075484666
deepc <dpokhriyal624@gmail.com>
2025-02-05 13:11:25 +0530

    Update

--- lib/devpage.dart
+++ lib/devpage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 class Devpage extends StatelessWidget {
 
@@ -22,39 +23,40 @@ class Devpage extends StatelessWidget {
                   child: Column(
                     children: [
                       Container(
-                        padding: EdgeInsets.only(top: 30),
+                        padding: EdgeInsets.only(top: 50),
                         child:Image.asset('assets/images/mylogobg.png'),
                         ),
+                        SizedBox(height: 30,),
                         Container( padding: EdgeInsets.all(20),
-                         child: Column(crossAxisAlignment: CrossAxisAlignment.start,
+                           child: Column(crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
                             Text('Dear Users -',style: TextStyle(fontFamily: 'nexalight',fontSize: 23,color: Colors.lightBlueAccent ),),
                             SizedBox(height: 25,),
                             Text('Welcome you to our application! ,We are excited to have you here! '
                                   '\nDeveloped by Team Celestial \nour application is built using the Flutter framework and Dart programming language to provide you with a seamless and enjoyable experience.'
                                   ' \n\nThank you for choosing us!',style: TextStyle(fontFamily: 'nexalight',fontSize: 15,color: Colors.white ),),
-                            SizedBox(height: 20,),
+                            SizedBox(height: 50,),
                             Divider(),
                             SizedBox(height: 16,),
                             Image.asset('assets/images/devlogo.png'),
                             SizedBox(height: 16,),
                             Divider(),
-                            SizedBox(height: 30,),
+                            SizedBox(height: 50,),
                             Container(
                               child: Row(
                                 children: [
-                                  Image.asset('assets/images/man.png',height: 70,),
-                                  SizedBox(width: 70,),
+                                  Image.asset('assets/images/deeppro.png',height: 70,),
+                                  SizedBox(width: 60,),
                                   Text('Deepak Pokhriyal',style: TextStyle(fontFamily: 'nexalight',fontSize: 25,color: Colors.lightBlueAccent),),
                                 ],
                               ),
                             ),
-                            SizedBox(height: 15,),
+                            SizedBox(height: 35,),
                             RichText(text:  TextSpan(
                               children: [
                                     TextSpan(
                                       text:"Hello, I'm Deepak Pokhriyal, currently pursuing a diploma in Information Technology 6th Semester. "
-      "As a member of the Celestial Group, my technical expertise is split between ", style: TextStyle(fontFamily: 'nexalight',fontSize: 16,color: Colors.white,letterSpacing: 1,wordSpacing: 1),
+                                              "As a member of the Celestial Group, my technical expertise is split between ", style: TextStyle(fontFamily: 'nexalight',fontSize: 16,color: Colors.white,letterSpacing: 1,wordSpacing: 1),
                                     ),
                                     TextSpan(
                                     text: 'frontend (55%) ', style: TextStyle(fontFamily: 'nexaheavy',fontSize: 16,color: Colors.green.shade400,letterSpacing: 1,wordSpacing: 1),
@@ -74,18 +76,19 @@ class Devpage extends StatelessWidget {
                               ]
                             )
                             ),
-                            SizedBox(height: 40),
+                            SizedBox(height: 70),
                             Container(
                               child: Row(
                                 children: [
                                   Text('Shivanshi Mishra',style: TextStyle(fontFamily: 'nexalight',fontSize: 25,color: Colors.lightBlueAccent),),
-                                  SizedBox(width: 70,),
-                                  Image.asset('assets/images/woman.png',height: 70,),
+                                  SizedBox(width: 65,),
+                                  Image.asset('assets/images/shivipro-modified.png',height: 70,),
                                 ],
                               ),
                             ),
                             SizedBox(height: 15,),
-                            RichText(text:  TextSpan(
+                            RichText(text:
+                              TextSpan(
                                 children: [
                                   TextSpan(
                                     text:"Hello, I'm Shivanshi Mishra, currently pursuing a diploma in Information Technology 6th Semester. "
@@ -111,13 +114,33 @@ class Devpage extends StatelessWidget {
                             ),
                             SizedBox(height: 10,),
                             Divider(),
-
+                            Row(
+                              children: [
+                                Text(' Developer\'s Profile : ', style: TextStyle(fontFamily: 'nexalight',fontSize: 18,color: Colors.greenAccent,letterSpacing: 1,wordSpacing: 1),),
+                                  GestureDetector(
+                                  onTap: () {
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                  SnackBar(content: Text('Image Tapped!')),
+                                  );
+                                  },
+                                  child: Image.asset('assets/images/tap1.png', // Replace with your image URL
+                                  width: 150,
+                                  height: 50,
+                                  ),
+                                  ),
+                              ],
+                            ),
+                            Divider(),
+                            Container(
+                              alignment: Alignment.center,
+                              child:
+                              Text('Contributes',style: TextStyle(fontFamily: 'nexaheavy',fontSize: 25,color: Colors.lightBlueAccent),),
+                            )
                           ],
                         ),
                         )
                     ],
                   ),
-
                 ),
               ],
             )

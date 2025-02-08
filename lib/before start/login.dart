240b7a000ce09669bd76d8e32269fd56aa068f50
deepc <dpokhriyal624@gmail.com>
2025-02-08 10:56:07 +0530

    Update

---
+++ lib/before start/login.dart
@@ -0,0 +1,201 @@
+import 'package:educationapk/before%20start/signup.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:educationapk/before%20start/startingpage.dart';
+
+class MyLogin extends StatelessWidget {
+
+  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+            backgroundColor: Colors.white,
+            body: Stack(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage('assets/backgrounds/dback22.jpg'), // Path to your background image
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  ListView( shrinkWrap: true,
+                    children: [
+                      Container(
+                          margin: EdgeInsets.fromLTRB(15, 30, 270, 0),
+                          child: Icon(Icons.arrow_back_ios,size: 30,)
+                      ),
+
+                      SizedBox(height: 15,),
+                      Container(
+                        padding: EdgeInsets.only(right: 30,left: 30),
+                        child: Text('Welcome\nGlad to see you.', style: TextStyle( color: Colors.black, fontSize: 40, fontFamily: 'sans-serif-thin'),),
+                      ),
+                      SizedBox(height: 20,),
+                      SingleChildScrollView(
+                        child: Container(
+                          padding: EdgeInsets.only(right: 30,left: 30),
+                          child: Column(
+                            children: [
+                              TextField( cursorColor: Colors.black,style: TextStyle( fontFamily: 'sans-serif-light'),
+                                controller: usernameController,
+                                decoration: InputDecoration(
+                                  fillColor: Colors.pink,
+                                  hintText: 'Enter your Name',
+                                  border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(35)
+                                  ),
+                                  focusedBorder: OutlineInputBorder(
+                                      borderSide: BorderSide(color: Colors.blue),// Focused border color
+                                      borderRadius: BorderRadius.circular(35)
+                                  ),
+                                ),
+                              ),
+                              SizedBox(height: 30,),
+                              TextField(style: TextStyle( fontFamily: 'sans-serif-light'),
+                                controller: passwordController,
+                                cursorColor: Colors.black,
+                                obscureText: true,
+                                decoration: InputDecoration(
+                                  fillColor: Colors.grey[100],
+                                  hintText: 'Enter your Password',
+                                  border: OutlineInputBorder( // Unfocused border color
+                                      borderRadius: BorderRadius.circular(35)
+                                  ),
+                                  focusedBorder: OutlineInputBorder(
+                                      borderSide: BorderSide(color: Colors.blue),// Focused border color
+                                      borderRadius: BorderRadius.circular(35)
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                padding: EdgeInsets.only(left: 215,top: 20),
+                                child: Text('Forget Password?',style: TextStyle(color: Colors.black,fontSize: 12,fontFamily: 'sans-serif-light'),textAlign: TextAlign.right,)                     ,
+                              ),
+
+                              SizedBox(height: 25,),
+                              SizedBox(
+                                height: 50,
+                                width: 450,
+                                child: ElevatedButton(
+                                  style: ElevatedButton.styleFrom(
+                                    foregroundColor: Colors.black, backgroundColor: Colors.black, // Set the text color here
+                                  ),  onPressed: () {
+                                    login(context);
+                                  // Get.to(()=>MyHomePage());
+                                },
+                                  child: Text('Login',style: TextStyle(color: Colors.white, fontSize: 16,fontFamily: 'sans-serif-light'),),
+                                ),
+                              ),
+                              SizedBox(height: 25,),
+                              Container(
+                                padding: EdgeInsets.only(left: 12,top: 20),
+                                child: Text('Or Login with',style: TextStyle(color: Colors.black, fontSize: 16,fontFamily: 'sans-serif-light'),textAlign: TextAlign.right,)                     ,
+                              ),
+                              SizedBox(height: 39,),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  SizedBox(
+                                    height: 46,
+                                    child: ElevatedButton(
+                                      style: ElevatedButton.styleFrom(
+                                        foregroundColor: Colors.black, backgroundColor: Colors.white, // Set the text color here
+                                      ),
+                                      onPressed: () {},
+                                      child: Image.asset('assets/images/facebook.png', height: 30,),
+                                    ),
+                                  ),
+                                  SizedBox(width: 30,),
+                                  SizedBox(
+                                    height: 46,
+                                    child: ElevatedButton(
+                                      onPressed: () async {
+                                        User? user = await signInWithGoogle();
+                                        if (user != null) {
+                                          Get.to(()=>MyHomePage());
+                                          print('User  signed in: ${user.displayName}');
+                                        }
+                                      },
+                                      child: Image.asset('assets/images/search.png', height: 29,),
+                                    ),
+
+
+                                  ),
+                                  SizedBox(width: 30,),
+                                  SizedBox(
+                                    height: 46,
+                                    child: ElevatedButton(
+                                      style: ElevatedButton.styleFrom(
+                                        foregroundColor: Colors.black, backgroundColor: Colors.white, // Set the text color here
+                                      ),
+                                      onPressed: () {},
+                                      child: Image.asset('assets/images/apple.png', height: 30,),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              SizedBox(height: 80,),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text('Don\'t Have an Account?', style: TextStyle(color: Colors.black,fontSize: 14,fontFamily: 'sans-serif-light'),),
+                                  SizedBox(width: 50,),
+
+                                  InkWell(
+                                    onTap: () {
+                                      Get.to(()=>MySignUpPage());
+                                    },
+                                    child: Text('Sign Up'),
+                                  )
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
+                ]
+            )
+        )
+    ;
+  }
+  void login(BuildContext context) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? storedUsername = prefs.getString("username");
+    String? storedPassword = prefs.getString("password");
+    print(storedPassword);
+    print(storedUsername);
+    if (usernameController.text == storedUsername && passwordController.text == storedPassword) {
+      // Navigate to home screen
+      Navigator.push(context, MaterialPageRoute(builder: (context)=>MyHomePage()));
+
+    }
+  }
+}
+
+Future<User?> signInWithGoogle() async {
+  final GoogleSignIn googleSignIn = GoogleSignIn();
+  final GoogleSignInAccount? googleUser  = await googleSignIn.signIn();
+  final GoogleSignInAuthentication? googleAuth = await googleUser ?.authentication;
+
+  final credential = GoogleAuthProvider.credential(
+    accessToken: googleAuth?.accessToken,
+    idToken: googleAuth?.idToken,
+  );
+
+  return (await FirebaseAuth.instance.signInWithCredential(credential)).user;
+}
+
+Future<void> signOut() async {
+  await FirebaseAuth.instance.signOut();
+  await GoogleSignIn().signOut();
+}
---
+++ lib/before start/signup.dart
@@ -0,0 +1,211 @@
+import 'package:educationapk/controllers/signupController.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class MySignUpPage extends StatefulWidget {
+  @override
+  State<MySignUpPage> createState() => _MySignUpPageState();
+}
+class _MySignUpPageState extends State<MySignUpPage> {
+  final SignupController controller=Get.put(tag: 'SignupController',SignupController());
+  TextEditingController usernameController=TextEditingController();
+  TextEditingController passwordController=TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/backgrounds/dback22.jpg'), // Path to your background image
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(top: MediaQuery.of(context).size.height*0.08,left: 30),
+            child: Text('Welcome !\nCreate you Account', style: TextStyle( color: Colors.black, fontSize: 38, fontFamily: 'sans-serif-thin'),),
+          ),
+
+          SingleChildScrollView(
+            child: Container(
+              padding: EdgeInsets.only(top: MediaQuery.of(context).size.height*0.25,right: 30,left: 30),
+              child: Column(
+                children: [
+                  TextField(
+                    controller: usernameController,
+                    cursorColor: Colors.black,style: TextStyle( fontFamily: 'sans-serif-light'),
+                    decoration: InputDecoration(
+                      fillColor: Colors.pink,
+                      hintText: 'Enter your Name',
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(35)
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.blue),// Focused border color
+                          borderRadius: BorderRadius.circular(35)
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 30,),
+                  TextField(style: TextStyle( fontFamily: 'sans-serif-light'),
+                    cursorColor: Colors.black,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      fillColor: Colors.grey[100],
+                      hintText: 'Enter your Email',
+                      border: OutlineInputBorder( // Unfocused border color
+                          borderRadius: BorderRadius.circular(35)
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.blue),// Focused border color
+                          borderRadius: BorderRadius.circular(35)
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 30,),
+                  TextField(style: TextStyle( fontFamily: 'sans-serif-light'),
+                    controller: passwordController,
+                    obscureText: true,
+                    cursorColor: Colors.black,
+                    decoration: InputDecoration(
+                      fillColor: Colors.grey[100],
+                      hintText: 'Enter your Password',
+                      border: OutlineInputBorder( // Unfocused border color
+                          borderRadius: BorderRadius.circular(35)
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.blue),// Focused border color
+                          borderRadius: BorderRadius.circular(35)
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 35,),
+                  SizedBox(
+                    width: double.infinity, // Full width
+                    child: ElevatedButton(
+                      onPressed: () {
+                        signup(context);
+                        // Navigator.pop(context);
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.black, // Set background color to black
+                        padding: EdgeInsets.symmetric(vertical: 15), // Button height
+                        textStyle: TextStyle(fontSize: 18, ), // Font size
+                      ),
+                      child: Text('Sign up',style: TextStyle(color: Colors.white,),),
+                    ),
+                  ),
+                  SizedBox(height: 25,),
+                  ElevatedButton(
+                    onPressed: () async {
+                      User? user = await signInWithGoogle();
+                      if (user != null) {
+                        print('User  signed in: ${user.displayName}');
+                      }
+                    },
+                    child: Text('Sign in with Google',style: TextStyle(color: Colors.black, fontSize: 16,fontFamily: 'sans-serif-light'),textAlign: TextAlign.right,),
+                  ),
+                  // Container(
+                  //   padding: EdgeInsets.only(left: 12,top: 20),
+                  //   child: Text('Or Sign up with',style: TextStyle(color: Colors.black, fontSize: 16,fontFamily: 'sans-serif-light'),textAlign: TextAlign.right,)                     ,
+                  // ),
+                  SizedBox(height: 39,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        height: 46,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            foregroundColor: Colors.black, backgroundColor: Colors.white, // Set the text color here
+                          ),
+                          onPressed: () {},
+                          child: Image.asset('assets/images/facebook.png', height: 30,),
+                        ),
+                      ),
+                      SizedBox(width: 30,),
+                      SizedBox(
+                        height: 46,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            foregroundColor: Colors.black, backgroundColor: Colors.white, // Set the text color here
+                          ),  onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => MySignUpPage(),),
+                          );
+                        },
+                          child: Image.asset('assets/images/search.png', height: 29,),
+                        ),
+                      ),
+                      SizedBox(width: 30,),
+                      SizedBox(
+                        height: 46,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            foregroundColor: Colors.black, backgroundColor: Colors.white, // Set the text color here
+                          ),
+                          onPressed: () {},
+                          child: Image.asset('assets/images/apple.png', height: 30,),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 80,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('Already Have an Account?', style: TextStyle(color: Colors.black,fontSize: 14,fontFamily: 'sans-serif-light'),),
+                      SizedBox(width: 50,),
+
+                      InkWell(
+                        onTap: (){
+                          Navigator.pop(context);
+                        },
+                        child: Text('Log in'),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  void signup(BuildContext context) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString("username", usernameController.text);
+    await prefs.setString("password", passwordController.text);
+
+    // Navigate back to login screen
+    Navigator.pop(context);
+  }
+}
+
+Future<User?> signInWithGoogle() async {
+  final GoogleSignIn googleSignIn = GoogleSignIn();
+  final GoogleSignInAccount? googleUser  = await googleSignIn.signIn();
+  final GoogleSignInAuthentication? googleAuth = await googleUser ?.authentication;
+
+  final credential = GoogleAuthProvider.credential(
+    accessToken: googleAuth?.accessToken,
+    idToken: googleAuth?.idToken,
+  );
+
+  return (await FirebaseAuth.instance.signInWithCredential(credential)).user;
+}
+
+Future<void> signOut() async {
+  await FirebaseAuth.instance.signOut();
+  await GoogleSignIn().signOut();
+}

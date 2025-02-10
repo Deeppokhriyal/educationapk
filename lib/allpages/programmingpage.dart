9f56026a815d7996134768e5b1f1d4edf5b6507a
deepc <dpokhriyal624@gmail.com>
2025-02-10 12:28:18 +0530

    Update

--- lib/allpages/programmingpage.dart
+++ lib/allpages/programmingpage.dart
@@ -1,7 +1,5 @@
 
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
 
 import '../programmingpage/c_page.dart';
 import '../programmingpage/html_page.dart';

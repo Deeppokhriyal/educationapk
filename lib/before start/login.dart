b8086db5c587abaf0cc9a5f5dddcb69bffc741a1
Deeppokhriyal <dpokhriyal624@gmail.com>
2025-04-11 12:18:21 +0530

    updated

--- lib/before start/login.dart
+++ lib/before start/login.dart
@@ -110,7 +110,7 @@ class _MyLoginState extends State<MyLogin> {
                           SizedBox(height: 20.h,),
                           Container(
                             padding: EdgeInsets.only(right: 30.w,left: 30.w),
-                            child: Text('Welcome\nGlad to see you.', style: TextStyle( color: Colors.black, fontSize: 25.sp, fontFamily: 'sans-serif-thin',fontWeight: FontWeight.bold),),
+                            child: Text('Welcome\nGlad to see you.', style: TextStyle( color: Colors.black, fontSize: 30.sp, fontFamily: 'sans-serif-thin',fontWeight: FontWeight.bold),),
                           ),
                           SizedBox(height: 20.h,),
                           SingleChildScrollView(
